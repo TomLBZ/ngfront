4147ff7580d5e3a96cf0f3f22387cf6b0b73591f
TomLBZ <libozhao142857@hotmail.com>
2025-01-07 12:22:13 +0800

    updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 import { PagesComponent } from './pages/pages.component';
 import { LoginComponent } from '../components/login/login';
 import { canActivateLoginGuard } from '../components/login/login.guard';
-import { routes as homeroutes } from './pages/pages.routes';
+import { routes as pageRoutes } from './pages/pages.routes';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'pages', component: PagesComponent, canActivate: [canActivateLoginGuard], children: homeroutes },
+    { path: 'pages', component: PagesComponent, canActivate: [canActivateLoginGuard], children: pageRoutes },
     { path: '**', redirectTo: '/login' },
 ];
