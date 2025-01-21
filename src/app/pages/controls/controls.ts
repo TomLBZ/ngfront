63bfd810578c320788b405c9ac8e36aa0fda9477
TomLBZ <libozhao142857@hotmail.com>
2025-01-21 09:51:28 +0800

    added controls

---
+++ src/app/pages/controls/controls.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+import { JoystickComponent } from "../../../components/joystick/joystick";
+import { ThrottleComponent } from "../../../components/throttle/throttle";
+import { Vec2 } from "../../../utils/vec/vec2";
+
+@Component({
+    selector: "page-controls",
+    standalone: true,
+    imports: [JoystickComponent, ThrottleComponent],
+    templateUrl: "./controls.html",
+    styleUrls: ["./controls.less"]
+})
+export class ControlsPage {
+    value: number = 0.0;
+    xy: Vec2 = new Vec2(0, 0);
+    onJoystickChanged(obj: any) {
+        this.xy = obj as Vec2;
+        console.log(obj);
+    }
+    onThrottleChanged(obj: any) {
+        this.value = obj as number;
+        console.log(obj);
+    }
+}
