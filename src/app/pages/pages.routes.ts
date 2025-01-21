3ad68758169e5b18e6172be789a518c66663b5ab
TomLBZ <libozhao142857@hotmail.com>
2025-01-21 09:51:34 +0800

    updated routes

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { PlaygroundComponent } from './playground/playground';
 import { ObjEditingPage } from './obj_editing/obj_editing';
 import { DropSelectPage } from './drop_select/drop_select';
+import { ControlsPage } from './controls/controls';
 
 export const routes : Routes = [
     {path: '', redirectTo: 'playground', pathMatch: 'full'},
     {path: 'playground', component: PlaygroundComponent},
     {path: 'object-editing', component: ObjEditingPage},
     {path: 'drop-select', component: DropSelectPage},
+    {path: 'controls', component: ControlsPage}
 ]
--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -12,7 +12,8 @@ export class PagesComponent {
   tabs = [
     {label: 'Playground', path: 'playground'},
     {label: 'Object Editing', path: 'object-editing'},
-    {label: 'Drop Select', path: 'drop-select'}
+    {label: 'Drop Select', path: 'drop-select'},
+    {label: 'Controls', path: 'controls'}
   ];
   private helpStrMultiline = `Help
   Multiline Help Item 1:
