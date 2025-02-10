0343a2db167f01a344d6bb3ce158217eb2465da7
TomLBZ <libozhao142857@hotmail.com>
2025-02-10 11:40:37 +0800

    1. added color presets for bmp 2. supported color from hex 3. supported smooth blending of sdf linecolor from both sides

--- src/utils/bmp/bmp.ts
+++ src/utils/bmp/bmp.ts
@@ -19,6 +19,25 @@ export class Color {
             Math.round(c1.a * weight + c2.a * (1 - weight))
         );
     }
+
+    static fromHex(hex: string) {
+        const r = parseInt(hex.slice(1, 3), 16);
+        const g = parseInt(hex.slice(3, 5), 16);
+        const b = parseInt(hex.slice(5, 7), 16);
+        const a = hex.length > 7 ? parseInt(hex.slice(7, 9), 16) : 255;
+        return new Color(r, g, b, a);
+    }
+
+    static Transparent = new Color(0, 0, 0, 0);
+    static Black = new Color(0, 0, 0, 255);
+    static White = new Color(255, 255, 255, 255);
+    static Red = new Color(255, 0, 0, 255);
+    static Green = new Color(0, 255, 0, 255);
+    static Blue = new Color(0, 0, 255, 255);
+    static Yellow = new Color(255, 255, 0, 255);
+    static Cyan = new Color(0, 255, 255, 255);
+    static Magenta = new Color(255, 0, 255, 255);
+    static Gray = new Color(127, 127, 127, 255);
 }
 
 export class Bitmap {
@@ -78,7 +97,7 @@ export class Bitmap {
         }
     }
 
-    drawSDF(sdf: (p: Vec2) => number, lineColor: Color, fillColor: Color, epsilon = 0.02, flipy = true) {
+    drawSDF(sdf: (p: Vec2) => number, lineColor: Color, fillColor: Color, epsilon = 0.01, flipy = true) {
         for (let y = 0; y < this.height; y++) {
             for (let x = 0; x < this.width; x++) {
                 const xx = 2 * x / this.width - 1; // normalize x to [-1, 1]
@@ -89,9 +108,9 @@ export class Bitmap {
                 const lineColorWeight = Math.exp(-d * d / epsilon);
                 // blend between lineColor and fillColor
                 const color = Color.blend(lineColor, fillColor, lineColorWeight);
-                if (d <= epsilon) { // inside or on the boundary
-                    this.setPixel(x, y, color);
-                }
+                // blend between lineColor and transparent color
+                const colorOutside = Color.blend(lineColor, Color.Transparent, lineColorWeight);
+                this.setPixel(x, y, d <= epsilon ? color : colorOutside);
             }
         }
     }
