43308b3dcd8404393fbde4b662c1c1a90e19555c
TomLBZ <libozhao142857@hotmail.com>
2025-02-10 11:14:56 +0800

    1. Fixed the bug where refreshing the page will highlight the wrong tab

--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -1,39 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-pages',
-  standalone: true,
-  imports: [RouterOutlet, RouterModule],
-  templateUrl: './pages.html',
-  styleUrl: './pages.less'
+    selector: 'app-pages',
+    standalone: true,
+    imports: [RouterOutlet, RouterModule],
+    templateUrl: './pages.html',
+    styleUrl: './pages.less'
 })
-export class PagesComponent {
-  tabs = [
-    {label: 'Playground', path: 'playground'},
-    {label: 'Object Editing', path: 'object-editing'},
-    {label: 'Drop Select', path: 'drop-select'},
-    {label: 'Controls', path: 'controls'},
-    {label: 'Path Edit', path: 'path-edit'},
-    {label: 'Logs', path: 'logs'}
-  ];
-  selectedTabPath = 'playground';
-  private helpStrMultiline = `Help
-  Multiline Help Item 1:
-    Left: Do something.
-      Hello World.
-  Multiline Help Item 2: abcd.
-  `;
-  showHelp() {
-    alert(this.helpStrMultiline);
-  }
-  logOut() {
-    alert('Logging out...');
-    localStorage.removeItem('salt');
-    window.location.href = '/';
-  }
-  onLinkClicked(event: any) {
-    const path = event.target.pathname;
-    this.selectedTabPath = path.split('/').pop();
-  }
+export class PagesComponent implements OnInit {
+    tabs = [
+        {label: 'Playground', path: 'playground'},
+        {label: 'Object Editing', path: 'object-editing'},
+        {label: 'Drop Select', path: 'drop-select'},
+        {label: 'Controls', path: 'controls'},
+        {label: 'Path Edit', path: 'path-edit'},
+        {label: 'Logs', path: 'logs'}
+    ];
+    selectedTabPath = 'playground';
+    private helpStrMultiline = `Help
+    Multiline Help Item 1:
+      Left: Do something.
+        Hello World.
+    Multiline Help Item 2: abcd.
+    `;
+    ngOnInit(): void {
+        const path = window.location.pathname;
+        this.selectedTabPath = path.split('/').pop() as any;
+    }
+    showHelp() {
+        alert(this.helpStrMultiline);
+    }
+    logOut() {
+        alert('Logging out...');
+        localStorage.removeItem('salt');
+        window.location.href = '/';
+    }
+    onLinkClicked(event: any) {
+        const path = event.target.pathname;
+        this.selectedTabPath = path.split('/').pop();
+    }
 }
