037b7dd1abde042dce9317a3f1ad8597cc12d152
TomLBZ <libozhao142857@hotmail.com>
2025-02-10 11:41:16 +0800

    1. supported epsilon input for icongen 2. removed color presets

--- src/utils/bmp/icongen.ts
+++ src/utils/bmp/icongen.ts
@@ -1,18 +1,8 @@
 import { Bitmap, Color, SDF, Vec2 } from "./bmp";
 
 export class IconGen {
-    static red = new Color(255, 0, 0, 255);
-    static green = new Color(0, 255, 0, 255);
-    static blue = new Color(0, 0, 255, 255);
-    static yellow = new Color(255, 255, 0, 255);
-    static cyan = new Color(0, 255, 255, 255);
-    static magenta = new Color(255, 0, 255, 255);
-    static white = new Color(255, 255, 255, 255);
-    static black = new Color(0, 0, 0, 255);
-    static gray = new Color (127, 127, 127, 255);
-    static transparent = new Color(0, 0, 0, 0);
-    static epsilon = 0.01;
-    static bloat = 0.9;
+    static epsilon = 0.006;
+    static bloat = 0.94;
     static get r() { return (1 - IconGen.epsilon) * IconGen.bloat; }
     static polyPlaneVecs = [
         new Vec2(-1, -1),
@@ -20,20 +10,20 @@ export class IconGen {
         new Vec2(1, -1),
         new Vec2(0, -0.5)
     ];
-    static Circle(iconSize: number, lineColor: Color, fillColor: Color) {
+    static Circle(iconSize: number, lineColor: Color, fillColor: Color, epsilon: number = IconGen.epsilon) {
         const bmp = new Bitmap(iconSize, iconSize);
         const sdf = (p: Vec2) => {
             return SDF.circle(p, IconGen.r);
         };
-        bmp.drawSDF(sdf, lineColor, fillColor);
+        bmp.drawSDF(sdf, lineColor, fillColor, epsilon);
         return bmp;
     }
-    static Poly(iconSize: number, vecs: Array<Vec2>, lineColor: Color, fillColor: Color) {
+    static Poly(iconSize: number, vecs: Array<Vec2>, lineColor: Color, fillColor: Color, epsilon: number = IconGen.epsilon) {
         const bmp = new Bitmap(iconSize, iconSize);
         const sdf = (p: Vec2) => {
             return SDF.poly(p, vecs.map(v => v.Mul(IconGen.r)));
         };
-        bmp.drawSDF(sdf, lineColor, fillColor);
+        bmp.drawSDF(sdf, lineColor, fillColor, epsilon);
         return bmp;
     }
 }
