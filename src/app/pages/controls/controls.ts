efeb51a04de54994c7cc6546c4f655aa91c612a6
TomLBZ <libozhao142857@hotmail.com>
2025-02-11 17:02:00 +0800

    updated controls page

--- src/app/pages/controls/controls.ts
+++ src/app/pages/controls/controls.ts
@@ -13,12 +13,17 @@ import { Vec2 } from "../../../utils/vec/vec2";
 export class ControlsPage {
     value: number = 0.0;
     xy: Vec2 = new Vec2(0, 0);
+    sensitivity: number = 0.1;
     onJoystickChanged(obj: any) {
         this.xy = obj as Vec2;
         console.log(obj);
     }
     onThrottleChanged(obj: any) {
         this.value = obj as number;
+        console.log(obj);
+    }
+    onSensitivityChanged(obj: any) {
+        this.sensitivity = obj as number;
         console.log(obj);
     }
 }
