0df045296349914bf499583bbf311966bd61c87d
TomLBZ <libozhao142857@hotmail.com>
2025-02-24 16:36:11 +0800

    updated MapView to: 1. Use Markergroup instead of Markers directly 2. Use Path for traces 3. Use Cache and Color and new Events 4. Remove unused interfaces, attributes and local classes

--- src/components/mapview/mapview.ts
+++ src/components/mapview/mapview.ts
@@ -3,59 +3,11 @@ import { MapComponent, LayerComponent, GeoJSONSourceComponent,
     FeatureComponent, ImageComponent, PopupComponent } from "@maplibre/ngx-maplibre-gl";
 import { DropSelectComponent } from '../dropselect/dropselect';
 import { Map, MapLayerMouseEvent } from 'maplibre-gl';
-
-export interface Marker {
-    id: number;
-    lat: number;
-    lon: number;
-    hdg: number;
-    popupText: string;
-    iconSize: number;
-    iconData: Uint8Array;
-    trackTarget: number;
-}
-
-export class MapViewEvent {
-    lng: number = -1;
-    lat: number = -1;
-    private buttons: number = -1;
-    public get noButton(): boolean {
-        return this.buttons <= 0; // 0: no button
-    }
-    public get primaryButton(): boolean {
-        return (this.buttons & 1) > 0; // 1: primary button (usually left)
-    }
-    public get secondaryButton(): boolean {
-        return (this.buttons & 2) > 0; // 2: secondary button (usually right)
-    }
-    public get middleButton(): boolean {
-        return (this.buttons & 4) > 0; // 4: auxiliary button (usually middle or mouse wheel button)
-    }
-    public get fourthButton(): boolean {
-        return (this.buttons & 8) > 0; // 8: 4th button (typically the "Browser Back" button)
-    }
-    public get fifthButton(): boolean {
-        return (this.buttons & 16) > 0; // 16: 5th button (typically the "Browser Forward" button)
-    }
-    constructor(event?: MapLayerMouseEvent) {
-        if (event === undefined) return;
-        this.lng = event.lngLat.lng;
-        this.lat = event.lngLat.lat;
-        const button = event.originalEvent.button === 2 ? 2 :
-            event.originalEvent.button === 1 ? 4 :
-            event.originalEvent.button === 3 ? 8 :
-            event.originalEvent.button === 4 ? 16 : 0;
-        this.buttons = event.originalEvent.buttons | button;
-    }
-}
-
-export class MarkerEvent extends MapViewEvent {
-    idx: number = -1;
-    constructor(idx: number, event?: MapLayerMouseEvent) {
-        super(event);
-        this.idx = idx;
-    }
-}
+import { MarkerEvent, MapViewEvent } from './event';
+import { MarkerGroup } from '../../utils/marker/markergrp';
+import { Path } from '../../utils/path/path';
+import { Cache } from '../../utils/cache/cache';
+import { Color } from '../../utils/color/color';
 
 @Component({
     selector: 'mapview',
