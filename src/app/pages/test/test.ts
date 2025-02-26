2ca42f490d4868d0b524b31b1d2134d33ee07991
TomLBZ <libozhao142857@hotmail.com>
2025-02-26 17:07:22 +0800

    updated test page

--- src/app/pages/test/test.ts
+++ src/app/pages/test/test.ts
@@ -8,6 +8,9 @@ import { RTOS } from "../../../utils/rtos/rtos";
     templateUrl: 'test.html'
 })
 export class TestPage implements OnInit, OnDestroy {
+    private lift = 0.01; // changes pitch
+    private tilt = 0.01; // changes roll
+    private turn = 0.01; // changes yaw
     uniforms: UniformDict = {
         u_pitch: 0.25,
         u_roll: 0.25,
@@ -24,7 +27,16 @@ export class TestPage implements OnInit, OnDestroy {
         if (!this.shaderHost) return;
         this.shaderHost.drawFrame();
     }
-
+    private updateUniforms() {
+        if (!this.shaderHost) return;
+        const t_s = performance.now() / 1000;
+        const new_uniforms = {
+            u_pitch: (this.uniforms['u_pitch'] as number) + this.lift * t_s,
+            u_roll: (this.uniforms['u_roll'] as number) + this.tilt * t_s,
+            u_yaw: (this.uniforms['u_yaw'] as number) + this.turn * t_s,
+        }
+        this.uniforms = new_uniforms;
+    }
     constructor() {
         this._rtos.addTask(() => this.draw(), {
             name: "Draw",
