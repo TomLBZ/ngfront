26e58edaa56848aa5a41e516494c2eb142a7c49f
TomLBZ <libozhao142857@hotmail.com>
2025-02-26 09:58:12 +0800

    updated Flag to have more intuitive function names

--- src/utils/flag/flag.ts
+++ src/utils/flag/flag.ts
@@ -1,9 +1,26 @@
 export class Flag {
     private _flags: number = 0;
     public names: Array<string> = [];
-    getFlag(name: string): boolean {
+    public get(name: string): boolean {
         if (this.names.indexOf(name) < 0) return false;
         return (this._flags & (1 << this.names.indexOf(name))) > 0;
+    }
+    public set(name: string) {
+        this._setFlag(name, true);
+    }
+    public clear(name: string) {
+        this._setFlag(name, false);
+    }
+    public toggle(name: string) {
+        this._setFlag(name, !this.get(name));
+    }
+    public addNames(names: Array<string>) {
+        names.forEach(name => {
+            if (this.names.indexOf(name) < 0) this.names.push(name);
+        });
+    }
+    public toNumber(): number {
+        return this._flags;
     }
     private _setFlag(name: string, value: boolean) {
         const idx = this.names.indexOf(name);
@@ -11,26 +28,9 @@ export class Flag {
         if (value) this._flags |= 1 << idx;
         else this._flags &= ~(1 << idx);
     }
-    setFlag(name: string) {
-        this._setFlag(name, true);
-    }
-    clearFlag(name: string) {
-        this._setFlag(name, false);
-    }
-    toggleFlag(name: string) {
-        this._setFlag(name, !this.getFlag(name));
-    }
     constructor(names: Array<string>, value?: number) {
         this.names = names;
         if (value !== undefined) this._flags = value;
-    }
-    addNames(names: Array<string>) {
-        names.forEach(name => {
-            if (this.names.indexOf(name) < 0) this.names.push(name);
-        });
-    }
-    toNumber(): number {
-        return this._flags;
     }
     static Default = new Flag([]);
 }
