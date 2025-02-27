32e6aa54a93a1c0e40cb839d3ba59d3a7956bc65
TomLBZ <libozhao142857@hotmail.com>
2025-02-27 18:45:13 +0800

    updated test to send uniforms to the shader

--- src/app/pages/test/test.ts
+++ src/app/pages/test/test.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, OnInit, OnDestroy } from "@angular/core";
 import { WebGLShaderHostComponent, UniformDict } from "../../../components/webglshaderhost/webglshaderhost";
 import { RTOS } from "../../../utils/rtos/rtos";
+import { Earth, ObserverOnEarth } from "../../../utils/geo/geo";
 
 @Component({
     selector: 'page-test',
@@ -12,9 +13,14 @@ export class TestPage implements OnInit, OnDestroy {
     private tilt = 0.01; // changes roll
     private turn = 0.01; // changes yaw
     uniforms: UniformDict = {
-        u_pitch: 0.25,
-        u_roll: 0.25,
-        u_yaw: 0.25
+        u_time: 0,
+        u_campos: [0, 0, 0],
+        u_camdir: [0, 0, 1],
+        u_camright: [1, 0, 0],
+        u_camup: [0, 1, 0],
+        u_sundir: [1, 1, 1],
+        u_RE: 6371000,
+        u_RAU: 1.0,
     }
     private _rtos: RTOS = new RTOS({
         cycleIntervalMs: 100,
@@ -25,16 +31,37 @@ export class TestPage implements OnInit, OnDestroy {
     @ViewChild(WebGLShaderHostComponent) shaderHost?: WebGLShaderHostComponent;
     private draw() {
         if (!this.shaderHost) return;
+        this.updateUniforms();
         this.shaderHost.drawFrame();
     }
     private updateUniforms() {
         if (!this.shaderHost) return;
         const t_s = performance.now() / 1000;
+        const msInMin = 60 * 1000; // number of milliseconds in a minute
+        const dt = new Date(Math.round(Date.now() / msInMin) * msInMin); // round to the nearest minute
+        const sunVec = Earth.getSunPositionVector(dt); // updated only in a minute
+        const lng = 103.822872;
+        const lat = 1.364917;
+        const alt = 6000;
+        const observer = new ObserverOnEarth(lng, lat, alt); // updated each cycle
+        observer.transform(this.tilt, this.lift, this.turn); // updated each cycle
+        const camPos = observer.position;
+        const camDir = observer.front;
+        const camRight = observer.right;
+        const camUp = observer.up;
+        const rE = Earth.getRadius(lat);
+        const rAU = Earth.R_AU;
         const new_uniforms = {
-            u_pitch: (this.uniforms['u_pitch'] as number) + this.lift * t_s,
-            u_roll: (this.uniforms['u_roll'] as number) + this.tilt * t_s,
-            u_yaw: (this.uniforms['u_yaw'] as number) + this.turn * t_s,
+            u_time: t_s,
+            u_campos: camPos.div(1000.0).ToArray(),
+            u_camdir: camDir.ToArray(),
+            u_camright: camRight.ToArray(),
+            u_camup: camUp.ToArray(),
+            u_sundir: sunVec.ToArray(),
+            u_RE: rE,
+            u_RAU: rAU,
         }
+        console.log(new_uniforms.u_campos);
         this.uniforms = new_uniforms;
     }
     constructor() {
