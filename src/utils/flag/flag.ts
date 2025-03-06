e6492a8c818cdbe5356b11701807b29829baf3bb
TomLBZ <libozhao142857@hotmail.com>
2025-03-06 09:30:17 +0800

    support more names related operations and quick tests for the flags class

--- src/utils/flag/flag.ts
+++ src/utils/flag/flag.ts
@@ -14,13 +14,33 @@ export class Flag {
     public toggle(name: string) {
         this._setFlag(name, !this.get(name));
     }
+    public addName(name: string) {
+        if (this.names.indexOf(name) < 0) this.names.push(name);
+    }
     public addNames(names: Array<string>) {
-        names.forEach(name => {
-            if (this.names.indexOf(name) < 0) this.names.push(name);
-        });
+        names.forEach((name) => this.addName(name));
+    }
+    public searchNames(search: string): Array<string> {
+        return this.names.filter((name) => name.includes(search));
     }
     public toNumber(): number {
         return this._flags;
+    }
+    public indexOf(name: string): number {
+        return this.names.indexOf(name);
+    }
+    public get none(): boolean {
+        return this._flags === 0; // all bits are 0
+    }
+    public get any(): boolean {
+        return this._flags !== 0; // any bit is 1
+    }
+    public get all(): boolean {
+        return this._flags === (1 << this.names.length) - 1; // all bits are 1
+    }
+    public set all(value: boolean) {
+        if (value) this._flags = (1 << this.names.length) - 1;
+        else this._flags = 0;
     }
     private _setFlag(name: string, value: boolean) {
         const idx = this.names.indexOf(name);
