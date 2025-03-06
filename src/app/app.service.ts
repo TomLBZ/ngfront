6ddfceab7102ea19021b431cd412a1557d423eaa
TomLBZ <libozhao142857@hotmail.com>
2025-03-06 09:24:44 +0800

    created more api integration functions

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { env } from './app.config';
+import { APIDataCallback, APIErrorCallback } from '../utils/api/api';
+import { Cache } from '../utils/cache/cache';
+import { Flag } from '../utils/flag/flag';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
     private apiUrl = env.apiUrl;
+    apiDataCache: Cache<any> = new Cache<any>();
+    apiFlags: Flag = new Flag([]);
     constructor(private http: HttpClient) {}
 
     uniPost(op: string, payload: any): Observable<any> {
@@ -18,10 +23,56 @@ export class AppService {
         return this.http.get(`${this.apiUrl}`);
     }
 
-    call(op: string, next: (d: any) => void, data: any = {}, error: (e: any) => void = console.error): void {
+    call(op: string, next: APIDataCallback, data: any = {}, error: APIErrorCallback = console.error): void {
         this.uniPost(op, data).subscribe({
             next,
             error
         });
+    }
+
+    private validAPIResponse(d: any): boolean {
+        return d.hasOwnProperty("success") && d.hasOwnProperty("msg") && d.hasOwnProperty("data");
+    }
+    
+    callAPI(op: string, next: APIDataCallback, data: any = {}, error: APIErrorCallback = console.error): void {
+        this.uniPost(op, data).subscribe({
+            next: (d: any) => this.validAPIResponse(d) ? next(d.data ? d.data : d) : error(d),
+            error
+        });
+    }
+
+    callAPIWithCache(op: string, data: any = undefined, error: APIErrorCallback = console.error): void {
+        const dataStr = data !== undefined ? JSON.stringify(data) : "";
+        const fname = op + dataStr; // name to be used for flags
+        if (this.apiFlags.indexOf(fname) < 0) { // not in flags
+            this.apiFlags.addName(fname);
+        }
+        if (this.apiFlags.get(fname)) return; // already called
+        this.callAPI(op, (d: any) => {
+            this.apiDataCache.set(this.apiFlags.indexOf(fname), d);
+            this.apiFlags.set(fname);
+        }, data, error);
+    }
+
+    getAPIData(op: string) {
+        return this.apiDataCache.get(this.apiFlags.indexOf(op));
+    }
+
+    unsetFlags(ops: Array<string>) {
+        for (const op of ops) {
+            this.apiFlags.clear(op);
+        }
+    }
+
+    testFlags(ops: Array<string>) {
+        let b = true;
+        for (const op of ops) {
+            b = b && this.apiFlags.get(op);
+        }
+        return b;
+    }
+
+    searchFlags(searchStr: string): Array<string> {
+        return this.apiFlags.searchNames(searchStr);
     }
 }
