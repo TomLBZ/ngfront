8f58e6e250aa6cded33f548b495585ab91b46964
TomLBZ <libozhao142857@hotmail.com>
2025-03-07 17:37:58 +0800

    updated telemetry struct

--- src/utils/telemetry/telemetry.ts
+++ src/utils/telemetry/telemetry.ts
@@ -1,9 +1,9 @@
 export interface TelemetryAPIResponse {
     battery: number;
     home_pos: {
-        lat: string;
-        lon: string;
-        alt: string;
+        lat: number;
+        lon: number;
+        alt: number;
     };
     curr_pos: {
         lat: number;
@@ -24,31 +24,6 @@ export interface TelemetryAPIResponse {
     };
 }
 export class Telemetry {
-    // {
-    //     "battery": 12.4,
-    //     "home_pos": {
-    //         "lat": "1.363",
-    //         "lon": "103.822872",
-    //         "alt": "15"
-    //     },
-    //     "curr_pos": {
-    //         "lat": 1.3627777,
-    //         "lon": 103.8235965,
-    //         "alt": 482.504639,
-    //         "hdg": 198.441257,
-    //         "speed": 23.13,
-    //         "course": 196.3,
-    //         "climb": -0.126916,
-    //     },
-    //     "airspeed": 0,
-    //     "groundspeed": 0,
-    //     "throttle": 84,
-    //     "attitude": {
-    //         "roll": 0.590449,
-    //         "pitch": -2.717998,
-    //         "yaw": -0.019
-    //     }
-    // }
     id: number = 0;
     name: string = "";
     battery: number = 0;
@@ -72,9 +47,9 @@ export class Telemetry {
         this.id = id;
         this.name = name;
         this.battery = data.battery;
-        this.homeLat = parseFloat(data.home_pos.lat); // TODO: after API update, change to number
-        this.homeLon = parseFloat(data.home_pos.lon);
-        this.homeAlt = parseFloat(data.home_pos.alt);
+        this.homeLat = data.home_pos.lat;
+        this.homeLon = data.home_pos.lon;
+        this.homeAlt = data.home_pos.alt;
         this.currLat = data.curr_pos.lat;
         this.currLon = data.curr_pos.lon;
         this.currAlt = data.curr_pos.alt;
