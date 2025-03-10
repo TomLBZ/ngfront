c697e8f089da654f90e09bc885ce4e64be0362ec
TomLBZ <libozhao142857@hotmail.com>
2025-03-11 02:12:08 +0800

    implemented recursive skirts and auto tiling, removed downloading code

--- src/utils/api/maptiler.ts
+++ src/utils/api/maptiler.ts
@@ -1,11 +1,8 @@
-import { Downloader } from "./downloader";
-
+export class MapTile {
+    constructor(public url: string, public tl: [number, number], public br: [number, number]) {}
+}
 export class MapTiler {
     public constructor(public key: string) { }
-    public async getTile(z: number, x: number, y: number): Promise<ImageBitmap> {
-        const url = `https://api.maptiler.com/tiles/satellite-v2/${z}/${x}/${y}.jpg?key=${this.key}`;
-        return Downloader.downloadImage(url);
-    }
     public lnglat2xy(z: number, lng: number, lat: number): [number, number] {
         const zfactor = 1 << z;
         const d2r = Math.PI / 180;
@@ -19,14 +16,15 @@ export class MapTiler {
         const lat = 180 / Math.PI * Math.atan(Math.sinh(Math.PI * (1 - 2 * y / zfactor)));
         return [lng, lat];
     }
-    public async getTileByLngLat(z: number, lng: number, lat: number): Promise<ImageBitmap> {
-        const xy = this.lnglat2xy(lng, lat, z);
-        return this.getTile(z, xy[0], xy[1]);
-    }
-    public getBounds(z: number, x: number, y: number): [number, number, number, number] {
+    public getTileByXY(z: number, x: number, y: number): MapTile {
+        const url = `https://api.maptiler.com/tiles/satellite-v2/${z}/${x}/${y}.jpg?key=${this.key}`;
         const tl = this.xy2lnglat(z, x, y);
         const br = this.xy2lnglat(z, x + 1, y + 1);
-        return [tl[0], tl[1], br[0], br[1]];
+        return new MapTile(url, tl, br);
+    }
+    public getTileByLngLat(z: number, lng: number, lat: number): MapTile {
+        const xy = this.lnglat2xy(lng, lat, z);
+        return this.getTileByXY(z, xy[0], xy[1]);
     }
     public alt2z(alt: number) {
         const r = 6371000; // earth radius in meters
@@ -34,35 +32,28 @@ export class MapTiler {
         if (isNaN(_z)) return 0;
         return Math.max(0, Math.min(19, _z));
     }
-    public async autoTilesXYZ(x: number, y: number, z: number, skirts: number = 3): Promise<ImageBitmap[]> {
-        const tiles: ImageBitmap[] = [];
-        tiles.push(await this.getTile(z, x, y)); // center tile is always pushed
-        if (z === 1) { // append the other 3 tiles
-            for (let i = 0; i < 2; i++) {
-                for (let j = 0; j < 2; j++) {
-                    if (i === x && j === y) continue;
-                    tiles.push(await this.getTile(z, i, j));
-                }
+    private recursiveSkirts(z: number, lng: number, lat: number, skirts: number, tiles: MapTile[]): MapTile[] {
+        if (skirts === 0) return tiles; // base case, no skirts
+        if (z === 0) return tiles; // base case, no more zoom levels
+        const [x, y] = this.lnglat2xy(z, lng, lat);
+        const zfactor = 1 << z;
+        const maxX = Math.min(zfactor - 1, x + 1);
+        const maxY = Math.min(zfactor - 1, y + 1);
+        const minX = Math.max(0, x - 1);
+        const minY = Math.max(0, y - 1);
+        for (let i = minX; i <= maxX; i++) {
+            for (let j = minY; j <= maxY; j++) {
+                if (i === x && j === y) continue;
+                tiles.push(this.getTileByXY(z, i, j));
             }
-        } else { // append 3 tiles based on distance from center as skirts, then recursively call autoTilesXYZ
-            // TODO: FIND A BETTER WAY TO DO THIS
         }
-        return tiles;
+        return this.recursiveSkirts(z - 1, lng, lat, skirts - 1, tiles);
     }
-    public async autoTiles(lng: number, lat: number, alt: number, skirts: number = 3): Promise<ImageBitmap[]> {
+    public autoTiles(lng: number, lat: number, alt: number, skirts: number = 1): MapTile[] {
         const z: number = this.alt2z(alt);
-        const xy: [number, number] = this.lnglat2xy(z, lng, lat);
-        const tiles: ImageBitmap[] = [];
-        tiles.push(await this.getTile(z, xy[0], xy[1])); // center tile is always pushed
-        if (z === 1) { // append the other 3 tiles
-            for (let i = 0; i < 2; i++) {
-                for (let j = 0; j < 2; j++) {
-                    if (i === xy[0] && j === xy[1]) continue;
-                    tiles.push(await this.getTile(z, i, j));
-                }
-            }
-            // TODO: CHANGE ALGORITHM TO INCLUDE SKIRTS!
-        }
-        return tiles;
+        const [x, y] = this.lnglat2xy(z, lng, lat);
+        const tiles: MapTile[] = [];
+        tiles.push(this.getTileByXY(z, x, y)); // center tile is always pushed
+        return this.recursiveSkirts(z, lng, lat, skirts, tiles);
     }
 }
