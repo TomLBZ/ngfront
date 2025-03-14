8ecbd462dcdb28beaf01b7ce73e27ee2b7d6b637
TomLBZ <libozhao142857@hotmail.com>
2025-03-14 17:32:14 +0800

    added logging, in progress of fixing tiles

--- src/app/pages/test/test.ts
+++ src/app/pages/test/test.ts
@@ -7,9 +7,9 @@ import { ObserverOnEarth } from "../../../utils/geo/observer";
 import { Vec3 } from "../../../utils/vec/vec3";
 import { Queue } from "../../../utils/queue/q";
 import { MissedDeadlinePolicy } from "../../../utils/rtos/rtostypes";
-import { UniformDict, UniformTexture, UniformTextureArray, UniformVec2, UniformVec2Array, UniformVec3, UniformVec3Array } from "../../../utils/uniform/u";
+import { UniformDict, UniformTexture, UniformTextureArray, UniformVec2, UniformVec3, UniformVec3Array } from "../../../utils/uniform/u";
 import { KeyController } from "../../../utils/controller/keyctrl";
-import { MapTile, MapTiler } from "../../../utils/api/maptiler";
+import { MapTiler } from "../../../utils/api/maptiler";
 import { env } from "../../app.config";
 
 @Component({
@@ -58,8 +58,8 @@ export class TestPage implements OnInit, OnDestroy {
     private updatePos(): ObserverOnEarth {
         const observer = new ObserverOnEarth(this.lng, this.lat, this.alt); // updated each cycle
         observer.transform(this.tilt, this.lift, this.turn); // updated each cycle
-        if (this.keyCtrl.getKeyState("w")) observer.forward(1000); // move forward 1 km
-        if (this.keyCtrl.getKeyState("s")) observer.forward(-1000); // move backward 1 km
+        if (this.keyCtrl.getKeyState("w")) observer.forward(2000); // move forward 1 km
+        if (this.keyCtrl.getKeyState("s")) observer.forward(-2000); // move backward 1 km
         if (this.keyCtrl.getKeyState("a")) this.turn -= 0.1;
         if (this.keyCtrl.getKeyState("d")) this.turn += 0.1;
         if (this.keyCtrl.getKeyState("q")) this.tilt -= 1000;
@@ -74,21 +74,28 @@ export class TestPage implements OnInit, OnDestroy {
         if (this.keyCtrl.getKeyState("f")) this.alt *= 0.99;
         return observer;
     }
+    data = {alt: 0, tiles: 0, sider: 0, z: 0};
     private updateUniforms(observer: ObserverOnEarth) {
         const msInMin = 60 * 1000; // number of milliseconds in a minute
         const dt = new Date(Math.round(Date.now() / msInMin) * msInMin); // round to the nearest minute
         const sunVec = Earth.getSunPositionVector(dt); // updated only in a minute
-        // const aggressionFunc: Function = (r: number) => {
-        //     const compression = Math.pow(r / observer.alt, 1 / 2);
-        //     return compression * observer.alt; // aggression function
-        // };
-        // const estm_mp = observer.lookingAtLngLatAlt(aggressionFunc, true); // estimated midpoint of the view
-        // const tiledist = Math.max(estm_mp.z, observer.alt); // distance to the tile in meters
-        const dftx = new Array(8).fill(0).map((_, i) => new UniformTexture(`textures/day/${i + 1}.jpg`, i)); // day textures
-        const mts = this.mapTiler.autoTiles(this.lng, this.lat, this.alt, 1); // get the urls
+        const z = this.mapTiler.alt2z(this.alt); // zoom level
+        const scaleR = -this.lift * Math.tan(observer.estm_raylen); // scale factor for the observer's offset
+        const sideR = 0.5 * Math.pow(2, 23) * this.alt / observer.RE; // side length of tile in meters
+        const aggr: Function = (l: number) => {
+            const cl = Math.cos(this.lift);
+            if (cl === 0) return this.alt;
+            return l * cl;
+        }
+        const lkat = observer.lookingAtLngLatAlt(aggr, false); // looking at lng, lat, alt
+        const off = observer.offset(sideR); // offset from the observer
+        // const mts = this.mapTiler.autoTiles(this.lng, this.lat, this.alt); // get the urls
+        const mts = this.mapTiler.autoTiles(lkat.x, lkat.y, this.alt); // get the urls
+        // const mts = this.mapTiler.autoTiles(off.x, off.y, Math.max(off.z, this.alt)); // get the urls
         const textures = mts.map((t, i) => new UniformTexture(t.url, i)); // create textures
         const xyzs = mts.map((t) => new UniformVec3(t.xyz));
         const new_uniforms = {
+            u_fov: new UniformVec2([Math.PI / 3, Math.PI / 3]), // field of view of 60 degrees
             u_ses: new UniformVec2([1e-6, 1e-9]), // scale factors for earth and sun
             u_srd: new UniformVec2([SUNR, Earth.DS_AU * AU]), // sun radius and distance to the sun in meters
             u_epos: new UniformVec3(observer.E2O_p(new Vec3()).ToArray()), // earth position in observer frame
@@ -96,13 +103,19 @@ export class TestPage implements OnInit, OnDestroy {
             u_ex: new UniformVec3(observer.eX.ToArray()), // earth's front in observer frame
             u_ey: new UniformVec3(observer.eY.ToArray()), // earth's right in observer frame
             u_ez: new UniformVec3(observer.eZ.ToArray()), // earth's up in observer frame
-            u_dftx: new UniformTextureArray(dftx, 0), // textures of day, default textures
             u_tx: new UniformTextureArray(textures, 1), // textures of map tiles
             u_txyz: new UniformVec3Array(xyzs), // xyz of map tiles
             u_ntx: xyzs.length, // number of tiles
             u_gridl: 1, // grid level
         };
         this.uniforms = new_uniforms;
+        if (this.data.alt !== this.alt || this.data.tiles !== mts.length || this.data.sider !== sideR || this.data.z !== z) {
+            this.data.z = z;
+            this.data.sider = sideR;
+            this.data.alt = this.alt;
+            this.data.tiles = mts.length;
+            console.log(`Alt: ${this.alt.toFixed(0)}, Tiles: ${mts.length}, Side: ${sideR.toFixed(0)}, Z: ${z}`);
+        }
     }
     constructor() {
         this._rtos.addTask(() => this.draw(), {
