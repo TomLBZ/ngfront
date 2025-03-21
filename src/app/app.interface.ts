aff26b7e39d28044bfce098deeb2aa59eaf42ec8
TomLBZ <libozhao142857@hotmail.com>
2025-03-21 17:16:23 +0800

    added interfaces to app.interface.ts

---
+++ src/app/app.interface.ts
@@ -0,0 +1,32 @@
+export interface Waypoint {
+    alt: number;
+    lat: number;
+    lon: number;
+    toa: number;
+}
+export interface Mission {
+    id: number;
+    name: string;
+    description: string;
+    lead_id: number;
+    lead_path: Array<Waypoint>;
+    follower_ids: Array<number>;
+}
+export interface Telemetry {
+    id: number;
+    roll: number;
+    pitch: number;
+    yaw: number;
+    lat: number;
+    lon: number;
+    alt: number;
+    hdg: number;
+    agl: number;
+    speed: number;
+    course: number;
+    climb: number;
+    throttle: number;
+}
+export interface Telemetries {
+    [key: number]: Telemetry;
+}
