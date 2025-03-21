9a68af0e42a6e77ad0d9b188f0ea8aea3d24c0da
TomLBZ <libozhao142857@hotmail.com>
2025-03-21 17:36:19 +0800

    IN PROG: 1. updated interface imports 2. privatised fields and implemented getters for group vars 3. made fields readonly 4. implemented changes on launch and runtime settings and warnings / popups 5. init mission path on startup 6. fixed the bug where traces length is not updated in real time 7. fixed the bug where invalid telemetry data from the ws server will cause frontend to crash 8. early return for lead id changes without recreating all fields 9. [PROG] fix the bug where unknown reason causes hash to collide (might be in path or marker groups, or in segments)

--- src/app/pages/monitor/monitor.ts
+++ src/app/pages/monitor/monitor.ts
@@ -10,29 +10,17 @@ import { MissedDeadlinePolicy } from '../../../utils/rtos/rtostypes';
 import { AppService } from '../../app.service';
 import { DropSelectComponent } from '../../../components/dropselect/dropselect';
 import { OnceValue } from '../../../utils/once/once';
-import { APICallback, APIResponse } from '../../../utils/api/api';
-import { TelemetryAPIResponse, TelemetryInstance } from '../../../utils/telemetry/telemetry';
+import { APICallback, APIResponse } from '../../app.interface';
 import { Marker } from '../../../utils/marker/marker';
 import { ObjEditorComponent } from '../../../components/obj_editor/obj_editor';
 import { Path, PathStyle } from '../../../utils/path/path';
 import { Point } from '../../../utils/point/point';
 import { Cache } from '../../../utils/cache/cache';
-interface Waypoint {
-    alt: number;
-    lat: number;
-    lon: number;
-    toa: number;
-}
-interface Mission {
-    id: number;
-    name: string;
-    description: string;
-    lead_id: number;
-    lead_path: Array<Waypoint>;
-    follower_ids: Array<number>;
-}
-interface Settings {
+import { Waypoint, Mission, Telemetries, Telemetry } from '../../app.interface';
+interface LaunchSettings {
     fgEnable: boolean;
+}
+interface RuntimeSettings {
     traces: number;
     lead_id: number;
 }
@@ -44,7 +32,6 @@ interface Status {
     mrun: boolean;
     mdone: boolean;
 }
-
 @Component({
     selector: 'page-monitor',
     imports: [
@@ -54,26 +41,30 @@ interface Status {
     templateUrl: 'monitor.html'
 })
 export class MonitorPage implements OnInit, OnDestroy {
-    planeMgrp: MarkerGroup = new MarkerGroup(Icon.Poly(16, Icon.polyPlaneVecs, Color.Blue));
-    wpGrp: MarkerGroup = new MarkerGroup(Icon.Circle(16, Color.Magenta));
-    paths: Array<Path> = [];
-    apiKey: string = env.mapKey;
-    settings: Settings = { fgEnable: true, traces: 50, lead_id: 0 };
+    private readonly planeMgrp: MarkerGroup = new MarkerGroup(Icon.Poly(16, Icon.polyPlaneVecs, Color.Blue));
+    private readonly wpGrp: MarkerGroup = new MarkerGroup(Icon.Circle(16, Color.Magenta));
+    public get markerGroups(): Array<MarkerGroup> { return [this.planeMgrp, this.wpGrp]; }
+    private readonly mpath: Path = new Path(-1);
+    private readonly ppaths: Array<Path> = [];
+    public get paths(): Array<Path> { return [this.mpath, ...this.ppaths]; }
+    public get apiKey(): string { return env.mapKey; }
+    launchSettings: LaunchSettings = { fgEnable: false };
+    runtimeSettings: RuntimeSettings = { traces: 50, lead_id: 0 };
     status: Status = { bridge: false, simulator: false, algo: false, mstatus: "None", mrun: false, mdone: false };
-    telemetries: Array<TelemetryInstance> = [];
+    telemetries: Array<Telemetry> = [];
+    idRepr: Function = (o: any) => `ID: ${o.id}`;
     visibleTelemetryIndices: Array<number> = [];
     missions: Array<Mission> = [];
     selectedMission?: Mission = undefined;
     nameRepr: Function = (o: any) => o.name;
-    idRepr: Function = (o: any) => `ID: ${o.id}`;
     private websocket?: WebSocket;
-    private pointsCache: Cache<Array<Point>> = new Cache<Array<Point>>();
-    private colorsCache: Cache<Color> = new Cache<Color>();
+    private readonly pointsCache: Cache<Array<Point>> = new Cache<Array<Point>>();
+    private readonly colorsCache: Cache<Color> = new Cache<Color>();
     @ViewChild(OutboxComponent) outbox?: OutboxComponent;
-    private stateApis = ["br/health", "sim/health", "mission/health"];//, "algo/health"
-    private missionApis = ["mission/all", "mission/current"];
-    private isMissionInvalidated: OnceValue<boolean> = new OnceValue(false);
-    private _rtos: RTOS = new RTOS({
+    private readonly stateApis = ["br/health", "sim/health", "mission/health"];//, "algo/health"
+    private readonly missionApis = ["mission/all", "mission/current"];
+    private readonly isMissionInvalidated: OnceValue<boolean> = new OnceValue(false);
+    private readonly _rtos: RTOS = new RTOS({
         cycleIntervalMs: 100,
         continueAfterInterrupt: true,
         timeSlicePerCycle: true,
@@ -101,8 +92,9 @@ export class MonitorPage implements OnInit, OnDestroy {
         this.outbox.clear(str);
     }
     private onStatesUpdated() {
-        const rawms = this.svc.getAPIData("mission/health").mission_status;
-        const ms = rawms === undefined ? "NONE" : rawms === "STOPPED" ? "SIGLOST" : rawms;
+        const data = this.svc.getAPIData("mission/health").data;
+        const rawms = data !== null && data !== undefined ? data.mission_status : "NONE";
+        const ms = rawms === "STOPPED" ? "SIGLOST" : rawms;
         this.status.bridge = this.svc.getAPIData("br/health").success;
         this.status.simulator = this.svc.getAPIData("sim/health").success;
         this.status.algo = this.svc.getAPIData("algo/health").success;
@@ -115,7 +107,7 @@ export class MonitorPage implements OnInit, OnDestroy {
             this.websocket = undefined;
             this.selectedMission = undefined;
             this.wpGrp.clearMarkers();
-            this.paths = [];
+            this.mpath.clear();
         }
         this.svc.unsetFlags(this.stateApis);
     }
@@ -123,9 +115,14 @@ export class MonitorPage implements OnInit, OnDestroy {
         this.missionApis.forEach((api: string) => this.svc.callAPIWithCache(api, undefined, this.void))
     }
     private onRunningMissionChanged(m: Mission) {
-        if (m === undefined || !this.isValidMission(m)) return; // invalid
+        if (!this.isValidMission(m)) return; // invalid
         if (m === this.selectedMission) return; // unchanged
-        if (this.websocket !== undefined) { // close old websocket
+        if (this.status.mrun && m.id === this.selectedMission?.id) { // running same mission, only possible change is lead_id
+            this.selectedMission.lead_id = m.lead_id;
+            this.runtimeSettings.lead_id = m.lead_id;
+            return;
+        }
+        if (this.websocket !== undefined) { // different mission: close old websocket
             this.websocket.close();
             this.websocket = undefined;
         }
@@ -137,23 +134,21 @@ export class MonitorPage implements OnInit, OnDestroy {
             this.wpGrp.updateMarker(m);
             leadPoints.push(new Point(wp.lon, wp.lat));
         });
-        const leadPath = new Path(-1); // avoid collision with actual path
-        leadPath.color = Color.Orange;
-        leadPath.weight = 2;
-        leadPath.style = PathStyle.Dashed;
-        leadPath.setPoints(leadPoints);
-        this.paths = [leadPath]; // set the lead path to visualise
+        this.mpath.setPoints(leadPoints);
         this.selectedMission = m;
-        this.settings.lead_id = m.lead_id; // set lead id
+        this.runtimeSettings.lead_id = m.lead_id; // set lead id
         this.websocket = new WebSocket(env.wsUrl); // open new websocket to get telemetry
         this.websocket.onmessage = (e: MessageEvent) => this.onTelemetry(e);
         this.websocket.onclose = () => this.onSocketClosed();
     }
     private onMissionsUpdated() {
-        const missions = this.svc.getAPIData("mission/all");
-        if (missions?.missions_config) this.missions = missions.missions_config;
-        const current: Mission = this.svc.getAPIData("mission/current");
-        this.onRunningMissionChanged(current);
+        const mdata = this.svc.getAPIData("mission/all").data;
+        if (mdata !== null && mdata !== undefined) {
+            const ms = mdata.missions_config;
+            if (ms !== null && ms !== undefined) this.missions = ms;
+        }
+        const current: Mission = this.svc.getAPIData("mission/current").data;
+        if (current !== null || current !== undefined) this.onRunningMissionChanged(current);
         this.svc.unsetFlags(this.missionApis);
     }
     private onSocketClosed() {
@@ -161,12 +156,24 @@ export class MonitorPage implements OnInit, OnDestroy {
         this.telemetries = [];
         this.planeMgrp.clearMarkers();
         this.pointsCache.clear();
+        this.ppaths.splice(0, this.ppaths.length); // clear old paths
+    }
+    private isValidTelemetry(t: Telemetry): boolean {
+        const keys = ["roll", "pitch", "yaw", "lat", "lon", "alt", "hdg", "agl", "speed", "course", "climb", "throttle"];
+        return keys.every((k: string) => t.hasOwnProperty(k) && (t as any)[k] !== null && (t as any)[k] !== undefined);
     }
     private onTelemetry(e: MessageEvent) {
-        this.telemetries = TelemetryInstance.fromAPIResponse(JSON.parse(e.data) as TelemetryAPIResponse);
+        const telemetries: Telemetries = e.data !== null && e.data !== undefined ? JSON.parse(e.data) as Telemetries : {} as Telemetries;
+        this.telemetries = [];
+        Object.entries(telemetries).forEach(([k, v]: [string, Telemetry]) => {
+            if (!this.isValidTelemetry(v)) return; // invalid telemetry
+            v.id = parseInt(k);
+            this.telemetries.push(v);
+        });
+        if (this.telemetries.length === 0) return;
         this.planeMgrp.clearMarkers();
-        this.paths = [this.paths[0]]; // keep the lead path only
-        this.telemetries.forEach((t: TelemetryInstance) => {
+        this.ppaths.splice(0, this.ppaths.length); // clear old paths
+        this.telemetries.forEach((t: Telemetry) => {
             const m = new Marker(t.lat, t.lon, t.id);
             m.alt = t.alt;
             m.hdg = t.hdg;
@@ -177,20 +184,30 @@ export class MonitorPage implements OnInit, OnDestroy {
             const path = new Path(t.id);
             const points = this.pointsCache.get(t.id);
             points.push(new Point(t.lon, t.lat));
-            if (points.length > this.settings.traces) points.shift(); // keep only last N points
+            if (points.length > this.runtimeSettings.traces) points.splice(0, points.length - this.runtimeSettings.traces); // remove oldest points
             if (!this.colorsCache.has(t.id)) {
                 this.colorsCache.set(t.id, Color.Random());
             }
             path.color = this.colorsCache.get(t.id);
             path.weight = 1;
             path.setPoints(points);
-            this.paths.push(path);
+            const hashes = this.ppaths.map((p: Path) => p.hash);
+            if (!hashes.includes(path.hash)) this.ppaths.push(path);
+            else {
+                console.log(path.id, path.hash);
+                const idx = hashes.indexOf(path.hash);
+                this.ppaths[idx] = path;
+            }
         });
         if (this.selectedMission !== undefined && this.colorsCache.has(this.selectedMission.lead_id)) {
             this.planeMgrp.setColor(this.selectedMission.lead_id, Color.Red); // set lead plane Border
         }
     }
     constructor(private svc: AppService) {
+        this.mpath = new Path(-1);
+        this.mpath.color = Color.Orange;
+        this.mpath.weight = 2;
+        this.mpath.style = PathStyle.Dashed;
         this._rtos.addTask(() => this.fetchStates(), {
             priority: 1, // low priority
             intervalMs: 1000, // fetches states per one second
@@ -200,7 +217,6 @@ export class MonitorPage implements OnInit, OnDestroy {
         this._rtos.addInterrupt(() => this.isMissionInvalidated.value, () => this.updateMissions());
         this._rtos.addInterrupt(() => this.svc.testFlags(this.missionApis), () => this.onMissionsUpdated());
     }
-    
     ngOnInit(): void {
         this._rtos.start();
         this.isMissionInvalidated.reset(true);
@@ -219,27 +235,34 @@ export class MonitorPage implements OnInit, OnDestroy {
     onMissionSelected(m: Mission) {
         this.selectedMission = m;
     }
-    onSettingsChanged(newSettings: Settings) {
-        if (newSettings.traces < 1) newSettings.traces = 1;
+    onLaunchSettingsChanged(newSettings: LaunchSettings) {
+        if (newSettings.fgEnable !== this.launchSettings.fgEnable) {
+            if (newSettings.fgEnable) {
+                alert("Enabling FlightGear makes launching a mission takes very long, please be patient. We recommend disabling this option.");
+            }
+            this.svc.callAPI("sim/fgenable", this.void, { fg_enable: newSettings.fgEnable }, this.popup);
+        }
+        this.launchSettings = newSettings;
+    }
+    onRuntimeSettingsChanged(newSettings: RuntimeSettings) {
+        if (newSettings.traces < 1) newSettings.traces = 1; // minimum traces is one
         if (this.selectedMission !== undefined) {
             const all_ids = [this.selectedMission.lead_id, ...this.selectedMission.follower_ids];
             if (!all_ids.includes(newSettings.lead_id)) { // invalid lead id
-                newSettings.lead_id = this.selectedMission.lead_id;
+                newSettings.lead_id = this.selectedMission.lead_id; // reset to old lead id
             }
-        } else if (newSettings.lead_id < 0) newSettings.lead_id = 0;
-        if (newSettings.fgEnable !== this.settings.fgEnable) {
-            this.svc.callAPI("sim/fgenable", this.void, { fg_enable: newSettings.fgEnable }, this.popup);
         }
-        if (newSettings.lead_id !== this.settings.lead_id) {
+        if (newSettings.lead_id !== this.runtimeSettings.lead_id) { // lead id changed
+            const backup_settings = this.runtimeSettings;
             this.svc.callAPI("mission/changelead", (d) => {
                 if (d.success) this.invalidate(d);
-                else this.popup(d);
+                else this.runtimeSettings = backup_settings;
             }, newSettings.lead_id, (d) => {
                 this.popup(d);
-                newSettings.lead_id = this.settings.lead_id; // revert
+                this.runtimeSettings = backup_settings;
             });
         }
-        this.settings = newSettings;
+        this.runtimeSettings = newSettings;
     }
     onLaunch() {
         this.svc.callAPI("mission/start", this.invalidate, this.selectedMission!.id, this.popup);
