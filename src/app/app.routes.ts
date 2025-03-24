d3db85becab7d8e8a9c215b5bc7b061f124b2f60
TomLBZ <libozhao142857@hotmail.com>
2025-03-24 09:48:42 +0800

    disabled login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { PagesComponent } from './pages/pages';
-import { LoginComponent, loginGuard } from '../components/login/login';
+// import { LoginComponent, loginGuard } from '../components/login/login';
 import { routes as pageRoutes } from './pages/pages.routes';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'pages', component: PagesComponent, canActivate: [loginGuard], children: pageRoutes },
-    { path: '**', redirectTo: '/login' },
+    // { path: 'login', component: LoginComponent },
+    // { path: 'pages', component: PagesComponent, canActivate: [loginGuard], children: pageRoutes },
+    // { path: '**', redirectTo: '/login' },
+    { path: 'pages', component: PagesComponent, children: pageRoutes },
+    { path: '**', redirectTo: '/pages' },
 ];
