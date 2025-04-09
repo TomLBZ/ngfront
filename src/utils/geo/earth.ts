d62c7e12a212946b62f73fe801d8499e02522271
TomLBZ <libozhao142857@hotmail.com>
2025-04-09 17:47:18 +0800

    updated earth to composition instead of OOP, and moved old code to old file

--- src/utils/geo/earth.ts
+++ src/utils/geo/earth.ts
@@ -1,104 +1,101 @@
-import { Dates } from "../date/dates";
-import { Circle3D } from "../geom/circle";
-import { Plane3D } from "../geom/plane";
-import { Vec2 } from "../vec/vec2";
-import { Vec3 } from "../vec/vec3";
-import { eclipticToEquatorial, julianDayToGMSTDegrees, julianDayToSunEcliptic, wrapDegrees } from "./geo";
+import { EarthSurfacePoint, GeoRadarData, RectangularCoords } from "./types";
 
-// assumes earth is at the origin with north pole along the z-axis and the prime meridian along the x-axis.
-export class Earth {
-    public static readonly R: number = 6371e3; // meters
-    public static readonly RE: number = 6378137.0; // meters
-    public static readonly RP: number = 6356752.3; // meters
-    public static readonly C: number = 2 * Math.PI * this.R; // meters
-    public static readonly Tilt: number = 23.44 * Math.PI / 180; // radians
-    public static readonly North: Vec3 = new Vec3(0, 0, 1);
+export const EER = 6378137.0; // meters, Earth equatorial radius
+export const EPR = 6356752.3142; // meters, Earth polar radius
 
-    // degrees to radians
-    private static d2r(d: number): number {
-        return d * Math.PI / 180;
-    }
-    // computes the accurate radius at a latitude.
-    public static getRadius(lat: number): number {
-        lat = this.d2r(Math.abs(lat)); // convert to radians. lat is -90 to 90.
-        const cosLat = Math.cos(lat);
-        const sinLat = Math.sin(lat);
-        const a = this.RE * this.RE * cosLat;
-        const b = this.RP * this.RP * sinLat;
-        const c = this.RE * cosLat;
-        const d = this.RP * sinLat;
-        return Math.sqrt((a * a + b * b) / (c * c + d * d));
-    }
-    // returns the position of a point on the surface of the earth given its longitude, latitude, and altitude.
-    public static getPosition(lngDeg: number, latDeg: number, alt: number): Vec3 {
-        const lng = this.d2r(lngDeg);
-        const lat = this.d2r(latDeg);
-        const cosLat = Math.cos(lat);
-        const sinLat = Math.sin(lat);
-        const cosLng = Math.cos(lng);
-        const sinLng = Math.sin(lng);
-        const r = this.getRadius(lat) + alt; // accurate radius at latitude + altitude
-        const x = r * cosLat * cosLng;
-        const y = r * cosLat * sinLng;
-        const z = r * sinLat;
-        return new Vec3(x, y, z);
-    }
-    // returns the lng and lat and alt of a point on the surface of the earth given its position vector.
-    public static getLngLatAlt(p: Vec3): Vec3 {
-        const r = p.Len();
-        const lng = Math.atan2(p.y, p.x);
-        const lat = Math.asin(p.z / r);
-        return new Vec3(lng * 180 / Math.PI, lat * 180 / Math.PI, r - this.getRadius(lat));
-    }
-    // returns the north vector on a plane perpendicular to the surface of the earth at a given coordinate.
-    public static getNorthAtPos(p: Vec3): Vec3 { // p is the position vector
-        return p.Cross(this.North).Cross(p).Norm();
-    }
-    // returns the intersection of a plane with Earth, assuming that Earth is a sphere at the origin
-    public static getIntersection(pl: Plane3D): Circle3D | undefined {
-        const d = Math.abs(pl.d); // distance from the origin
-        if (d > this.R) return undefined; // plane does not intersect Earth as it is outside the sphere
-        const r = Math.sqrt(this.R * this.R - d * d); // radius of the intersection circle
-        const cent = pl.n.mul(d).Neg(); // center of the circle
-        return new Circle3D(cent, pl.n, r);
-    }
-    private static _date: Date;
-    public static JD: number;
-    public static sunEclipticCoords: {lambdaDeg: number, DS_AU: number, epsilonDeg: number};
-    public static equatorialCoords: {alphaDeg: number, deltaDeg: number};
-    public static GMSTDeg: number;
-    private static setParams(date: Date) {
-        if (this._date === undefined || date !== this._date) { // only set if date has changed or not set
-            this._date = date;
-            this.JD = Dates.DateToJulianDay(date);
-            this.sunEclipticCoords = julianDayToSunEcliptic(this.JD);
-            this.equatorialCoords = eclipticToEquatorial(this.sunEclipticCoords.lambdaDeg, this.sunEclipticCoords.epsilonDeg);
-            this.GMSTDeg = julianDayToGMSTDegrees(this.JD);
-        }
-    }
-    public static getSubSolarPoint(date: Date) {
-        this.setParams(date);
-        const GHA = wrapDegrees(this.GMSTDeg - this.equatorialCoords.alphaDeg); // Greenwich Hour Angle
-        const latDeg = this.equatorialCoords.deltaDeg;
-        let lngDeg = -GHA;
-        lngDeg = ((lngDeg + 180 + 360) % 360) - 180; // convert to -180 to 180
-        return { latDeg, lngDeg };
-    }
-    public static getSunPositionVector(date: Date): Vec3 {
-        this.setParams(date);
-        const alpha = this.equatorialCoords.alphaDeg * Math.PI / 180;
-        const delta = this.equatorialCoords.deltaDeg * Math.PI / 180;
-        // inertial frame
-        const xEq = this.sunEclipticCoords.DS_AU * Math.cos(delta) * Math.cos(alpha);
-        const yEq = this.sunEclipticCoords.DS_AU * Math.cos(delta) * Math.sin(alpha);
-        const zEq = this.sunEclipticCoords.DS_AU * Math.sin(delta);
-        // earth frame (rotating with the earth)
-        const GMSTrad = this.GMSTDeg * Math.PI / 180;
-        const cosT = Math.cos(-GMSTrad);
-        const sinT = Math.sin(-GMSTrad);
-        const xEcef = cosT * xEq - sinT * yEq;
-        const yEcef = sinT * xEq + cosT * yEq;
-        const zEcef = zEq;
-        return new Vec3(xEcef, yEcef, zEcef).Norm();
-    }
+export function geodeticToECEF(lng: number, lat: number, alt: number): RectangularCoords {
+    const phi = lat * Math.PI / 180;
+    const lambda = lng * Math.PI / 180;
+    const cosPhi = Math.cos(phi);
+    const sinPhi = Math.sin(phi);
+    const a2 = EER * EER;
+    const b2 = EPR * EPR;
+    const e2 = (a2 - b2) / a2;
+    const N = EER / Math.sqrt(1 - e2 * sinPhi * sinPhi);
+    const x = (N + alt) * cosPhi * Math.cos(lambda);
+    const y = (N + alt) * cosPhi * Math.sin(lambda);
+    const z = ((1 - e2) * N + alt) * sinPhi;
+    return {x, y, z} as RectangularCoords;
+}
+export function ecefToGeodetic(x: number, y: number, z: number): EarthSurfacePoint {
+    const a2 = EER * EER; // a = EER
+    const b2 = EPR * EPR; // b = EPR
+    const e2 = (a2 - b2) / a2;
+    const ep2 = (a2 - b2) / b2; // e'2
+    const e4 = e2 * e2;
+    const p2 = x * x + y * y;
+    const p = Math.sqrt(p2);
+    const z2 = z * z;
+    const F = 54 * b2 * z2;
+    const G = p2 + (1 - e2) * z2 - e2 * (a2 - b2);
+    const G2 = G * G;
+    const c = (e4 * F * p2) / (G * G2);
+    const s = Math.pow(1 + c + Math.sqrt(c * c + 2 * c), 1 / 3);
+    const k = s + 1 + 1 / s;
+    const k2 = k * k;
+    const P = F / (3 * k2 * G2);
+    const Q2 = 1 + 2 * e4 * P;
+    const Q = Math.sqrt(Q2);
+    const r0 = -(P * e2 * p) / (1 + Q) + Math.sqrt(0.5 * a2 * (1 + 1 / Q) - (P * (1 - e2) * z2) / (Q + Q2) - 0.5 * P * p2);
+    const _UV_param_root = p - e2 * r0;
+    const _UV_param = _UV_param_root * _UV_param_root;
+    const U = Math.sqrt(_UV_param + z2);
+    const V = Math.sqrt(_UV_param + (1 - e2) * z2);
+    const aV = EER * V;
+    const z0 = (b2 * z) / aV;
+    const phi = Math.atan((z + ep2 * z0) / p);
+    const lambda = Math.atan2(y, x);
+    const lng = lambda * 180 / Math.PI;
+    const lat = phi * 180 / Math.PI;
+    const alt = U * (1 - b2 / aV);
+    return { lng, lat, alt } as EarthSurfacePoint;
+}
+export function getECEFToENUMatrix(lng: number, lat: number) {
+    const phi = lat * Math.PI / 180;
+    const lambda = lng * Math.PI / 180;
+    const cosPhi = Math.cos(phi);
+    const sinPhi = Math.sin(phi);
+    const cosLambda = Math.cos(lambda);
+    const sinLambda = Math.sin(lambda);
+    return [
+        -sinLambda, cosLambda, 0,
+        -sinPhi * cosLambda, -sinPhi * sinLambda, cosPhi,
+        cosPhi * cosLambda, cosPhi * sinLambda, sinPhi
+    ];
+}
+export function getENUToECEFMatrix(lng: number, lat: number) {
+    const phi = lat * Math.PI / 180;
+    const lambda = lng * Math.PI / 180;
+    const cosPhi = Math.cos(phi);
+    const sinPhi = Math.sin(phi);
+    const cosLambda = Math.cos(lambda);
+    const sinLambda = Math.sin(lambda);
+    return [
+        -sinLambda, -sinPhi * cosLambda, cosPhi * cosLambda,
+        cosLambda, -sinPhi * sinLambda, cosPhi * sinLambda,
+        0, cosPhi, sinPhi
+    ];
+}
+export function getGeoRadarData(lng: number, lat: number, alt: number): GeoRadarData {
+    const pos = { lng, lat, alt } as EarthSurfacePoint;
+    const ecefPos = geodeticToECEF(lng, lat, alt);
+    const enuToEcefMatrix = getENUToECEFMatrix(lng, lat);
+    const ecefToEnuMatrix = getECEFToENUMatrix(lng, lat);
+    return { pos, ecefPos, enuToEcefMatrix, ecefToEnuMatrix } as GeoRadarData;
+}
+export function ecefToRadarEnu(ecef: RectangularCoords, radar: GeoRadarData): RectangularCoords {
+    const [x, y, z] = [ecef.x - radar.ecefPos.x, ecef.y - radar.ecefPos.y, ecef.z - radar.ecefPos.z]; // vector pointing from radar to target point
+    return {
+        x: radar.ecefToEnuMatrix[0] * x + radar.ecefToEnuMatrix[1] * y + radar.ecefToEnuMatrix[2] * z,
+        y: radar.ecefToEnuMatrix[3] * x + radar.ecefToEnuMatrix[4] * y + radar.ecefToEnuMatrix[5] * z,
+        z: radar.ecefToEnuMatrix[6] * x + radar.ecefToEnuMatrix[7] * y + radar.ecefToEnuMatrix[8] * z
+    };
+}
+export function radarEnuToEcef(enu: RectangularCoords, radar: GeoRadarData): RectangularCoords {
+    const {x, y, z} = enu;
+    return {
+        x: radar.enuToEcefMatrix[0] * x + radar.enuToEcefMatrix[1] * y + radar.enuToEcefMatrix[2] * z + radar.ecefPos.x,
+        y: radar.enuToEcefMatrix[3] * x + radar.enuToEcefMatrix[4] * y + radar.enuToEcefMatrix[5] * z + radar.ecefPos.y,
+        z: radar.enuToEcefMatrix[6] * x + radar.enuToEcefMatrix[7] * y + radar.enuToEcefMatrix[8] * z + radar.ecefPos.z
+    };
 }
