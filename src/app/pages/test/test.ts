1814116816ccea89b26eb47452341f283ed1f91e
TomLBZ <libozhao142857@hotmail.com>
2025-05-26 16:23:18 +0800

    uses real sun dir

--- src/app/pages/test/test.ts
+++ src/app/pages/test/test.ts
@@ -1,9 +1,10 @@
 import { Component, ViewChild, OnDestroy, AfterViewInit, ElementRef } from "@angular/core";
 import { RenderPipeline, RenderHelper, UniformRecord } from "../../../utils/gpu";
 import { Queue } from "../../../utils/src/ds/q";
-import { Attitude, GeodeticCoords, CoordsFrameType } from "../../../utils/geo";
+import { Attitude, GeodeticCoords, CoordsFrameType, GeoHelper } from "../../../utils/geo";
 import { AppService } from "../../app.service";
 import { GeoCam } from "../../../utils/src/geo/cam";
+import { dateToSunData, sunDataToSunPositionVectorEcef } from "../../../utils/src/geo/astro";
 
 @Component({
     selector: 'page-test',
@@ -15,9 +16,9 @@ export class TestPage implements AfterViewInit, OnDestroy {
     private _startTimeMs: number = 0;
     private _frameTimeQ: Queue<number> = new Queue<number>(32);
     private _lastFrameTime: number = Date.now();
-    private _running: boolean = false;
+    private _glRunning: boolean = false;
     private _lastIntFps: number = 0;
-    private _geoCoords: GeodeticCoords = [0, 0, 1000]; // longitude, latitude, altitude
+    private _geoCoords: GeodeticCoords = [103.844, 1.311, 1000]; // longitude, latitude, altitude
     private _attitude: Attitude = [0, 0, 0]; // roll, pitch, yaw
     private fpsText: string = "FPS: 0.00";
     private attText: string = "Attitude: [0, 0, 0]";
@@ -43,12 +44,12 @@ export class TestPage implements AfterViewInit, OnDestroy {
             this.setupPipeline(p);
             this._startTimeMs = Date.now();
             this._lastFrameTime = this._startTimeMs;
-            this._running = true;
+            this._glRunning = true;
             this.drawFrame();
         });
     }
     ngOnDestroy(): void {
-        this._running = false;
+        this._glRunning = false;
     }
     private setupPipeline(p: RenderPipeline): void {
         p.setPasses([
@@ -68,30 +69,33 @@ export class TestPage implements AfterViewInit, OnDestroy {
         ]), this._gl.ELEMENT_ARRAY_BUFFER), this._gl.UNSIGNED_SHORT, 6);
     }
     private drawFrame(): void {
-        if (!this._pipeline) return;
-        const resolution = [this.canvasRef.nativeElement.clientWidth, this.canvasRef.nativeElement.clientHeight];
-        const minres = Math.min(...resolution);
-        const scale = [resolution[0] / minres, resolution[1] / minres];
-        this.controllerUpdate();
-        const cam = new GeoCam(this._geoCoords, this._attitude, CoordsFrameType.ENU);
-        const epos = cam.ecefToCamFrame([0, 0, 0]); // earth position in camera frame
-        // const epos = [0, -16371000, 0]; // earth position in camera frame
-        const globalUniforms: UniformRecord = {
-            "u_scale": scale,
-        };
-        const uniforms: Record<string, UniformRecord> = {
-            "raymarch": {
-                "u_fov": [Math.PI / 3, Math.PI / 3], // field of view of 60 degrees
-                "u_minres": minres, // minimum resolution
-                "u_sundir": [Math.sqrt(3) / 2, 0, 0.5], // sun direction in observer frame
-                "u_epos": epos, // earth position in observer frame
-                "u_escale": 1e-6, // scale factors for earth and sun
-            },
-        };
-        this._pipeline.setGlobalUniforms(globalUniforms);
-        this._pipeline.renderAll(uniforms);
-        this.updateText();
-        if (this._running) requestAnimationFrame(() => this.drawFrame());
+        if (this._pipeline) {
+            const resolution = [this.canvasRef.nativeElement.clientWidth, this.canvasRef.nativeElement.clientHeight];
+            const minres = Math.min(...resolution);
+            const scale = [resolution[0] / minres, resolution[1] / minres];
+            this.controllerUpdate(); // update attitude and geodetic coordinates based on key presses
+            const cam = new GeoCam(this._geoCoords, this._attitude, CoordsFrameType.ENU);
+            const epos = cam.ecefToCamFrame([0, 0, 0]); // earth position in camera frame
+            const sunData = dateToSunData(new Date(Date.now()));
+            const sunVec = sunDataToSunPositionVectorEcef(sunData);
+            const sundir = GeoHelper.Normalize(cam.ecefToCamFrame(sunVec, false)); // sun direction in camera frame
+            const globalUniforms: UniformRecord = {
+                "u_scale": scale,
+            };
+            const uniforms: Record<string, UniformRecord> = {
+                "raymarch": {
+                    "u_fov": [Math.PI / 3, Math.PI / 3], // field of view of 60 degrees
+                    "u_minres": minres, // minimum resolution
+                    "u_sundir": sundir, // sun direction in observer frame
+                    "u_epos": epos, // earth position in observer frame
+                    "u_escale": 1e-6, // scale factors for earth and sun
+                },
+            };
+            this._pipeline.setGlobalUniforms(globalUniforms);
+            this._pipeline.renderAll(uniforms);
+            this.updateText();
+        }
+        if (this._glRunning) requestAnimationFrame(() => this.drawFrame());
         else this._pipeline.dispose();
     }
     private controllerUpdate(): void {
@@ -107,8 +111,8 @@ export class TestPage implements AfterViewInit, OnDestroy {
         if (this._svc.keyCtrl.getKeyState("ArrowDown")) this._geoCoords[1] -= 0.1; // move south
         if (this._svc.keyCtrl.getKeyState("ArrowLeft")) this._geoCoords[0] -= 0.1; // move west
         if (this._svc.keyCtrl.getKeyState("ArrowRight")) this._geoCoords[0] += 0.1; // move east
-        if (this._svc.keyCtrl.getKeyState("Shift")) this._geoCoords[2] -= 0.1; // move down
-        if (this._svc.keyCtrl.getKeyState(" ")) this._geoCoords[2] += 0.1; // move up
+        if (this._svc.keyCtrl.getKeyState("Shift")) this._geoCoords[2] -= 1000; // move down
+        if (this._svc.keyCtrl.getKeyState(" ")) this._geoCoords[2] += 1000; // move up
     }
     private fixedFloats(arr: number[], digits: number = 4): string[] {
         return arr.map((v) => v.toFixed(digits));
